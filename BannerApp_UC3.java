da178caad7d8fe233a7061e9125d7549434e1011
vinoth9598 <pvinoth9598@gmail.com>
2026-03-24 11:17:07 +0530

    feature/UC3_stringJoins

---
+++ BannerApp_UC3.java
@@ -0,0 +1,11 @@
+public class BannerApp_UC3 {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ","  ***  " + " " + "  ***  " + " " +"*******" + " ******"));
+        System.out.println(String.join(" "," ** ** " + " " + " ** ** " + " " +"*     *" + " *     "));
+        System.out.println(String.join(" ","**   **" + " " + "**   **" + " " +"*     *" + " *     "));
+        System.out.println(String.join(" ","**   **" + " " + "**   **" + " " +"*******" + " ******"));
+        System.out.println(String.join(" ","**   **" + " " + "**   **" + " " +"*      " + "      *"));
+        System.out.println(String.join(" "," ** ** " + " " + " ** ** " + " " +"*      " + "      *"));
+        System.out.println(String.join(" ","  ***  " + " " + "  ***  " + " " +"*      " + " ******"));
+    }
+}
