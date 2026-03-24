3d62010eb106ce9af066a78f9610a7e93e83c0b4
vinoth9598 <pvinoth9598@gmail.com>
2026-03-24 12:47:10 +0530

    feature/UC6_refactor

---
+++ BannerApp_UC6.java
@@ -0,0 +1,49 @@
+public class BannerApp_UC6 {
+    public static void main(String[] args) {
+        
+       
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        for(int i=0; i < oPattern.length; i++){
+            System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i] );
+        }
+    }
+
+    public static String[] getOPattern(){
+        return new String[] {
+            "   ***   ",
+            " **   ** ",
+            "**     **",
+            "**     **",
+            "**     **",
+            " **   ** ",
+            "   ***   "
+        };
+    }
+
+    public static String[] getSPattern(){
+        return new String[]{
+            "*********",
+            "*        ",
+            "*        ",
+            "*********",
+            "        *",
+            "        *",
+            "*********"
+        };
+    }
+
+    public static String[] getPPattern(){
+        return new String[] {
+            "*********",
+            "*       *",
+            "*       *",
+            "*********",
+            "*        ",
+            "*        ",
+            "*        "
+        };
+    }
+}
