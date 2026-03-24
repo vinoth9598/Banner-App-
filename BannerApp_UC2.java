1e38e10e507e3aa1ec37a6b26fc3b7c14ff29f93
vinoth9598 <pvinoth9598@gmail.com>
2026-03-24 11:05:04 +0530

    feature/UC2_printStatements

---
+++ BannerApp_UC2.java
@@ -0,0 +1,12 @@
+public class BannerApp_UC2 {
+    public static void main(String[] args){
+        System.out.println("  ***  " + " " + "  ***  " + " " +"*******" + " ******");
+        System.out.println(" ** ** " + " " + " ** ** " + " " +"*     *" + " *     ");
+        System.out.println("**   **" + " " + "**   **" + " " +"*     *" + " *     ");
+        System.out.println("**   **" + " " + "**   **" + " " +"*******" + " ******");
+        System.out.println("**   **" + " " + "**   **" + " " +"*      " + "      *");
+        System.out.println(" ** ** " + " " + " ** ** " + " " +"*      " + "      *");
+        System.out.println("  ***  " + " " + "  ***  " + " " +"*      " + " ******");
+
+    }
+}
